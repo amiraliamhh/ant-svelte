3e4faffb1300f4fb1ff5b7054c94717eba823bbb
amiraliamhh <amiraliamhh@gmail.com>
2019-12-07 23:47:38 +0330

    initial

---
+++ components/button/button.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let type = 'default'
+</script>
+
+<button class={ type } >
+    <slot />
+</button>
+
+<style>
+button {
+    border: none;
+}
+.default {
+    background-color: #1890ff;
+}
+</style>
